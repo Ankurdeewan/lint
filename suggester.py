062c18ef0a0bf3adbe8d2356f77eb2430c95776e
Haytham <ankurdeewan20+github@gmail.com>
2026-03-10 18:13:50 +0000

    Add CLI, resolver, suggester modules and restructure entrypoint

---
+++ suggester.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+def levenshtein(a: str, b: str) -> int:
+        if a == b:
+            return 0
+        if not a:
+            return len(b)
+        if not b:
+            return len(a)
+
+        if len(a) > len(b):
+            a, b = b, a
+
+        previous_row = list(range(len(a) + 1))
+
+        for i, bc in enumerate(b, start=1):
+            current_row = [i]
+            for j, ac in enumerate(a, start=1):
+                insert_cost = current_row[j - 1] + 1
+                delete_cost = previous_row[j] + 1
+                replace_cost = previous_row[j - 1] + (ac != bc)
+
+                current_row.append(min(insert_cost, delete_cost, replace_cost))
+
+            previous_row = current_row
+
+        return previous_row[-1]
